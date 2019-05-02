6ff26bf3e2996b1d0dd3217e58c710f0f1959d7b
Jason Liu <jasonliu0704@gmail.com>
2019-05-02 00:13:47 -0700

    basic complete

--- mail.php
+++ mail.php
@@ -2,11 +2,12 @@
     $to = 'demo@spondonit.com';
     $name = $_POST["name"];
     $email= $_POST["email"];
+    $message= $_POST["message"];
 
     $fp = fopen("contacts.txt", "a");
 
     //if (flock($fp, LOCK_EX)) {  // acquire an exclusive lock
-    fwrite($fp, $email." ".$name."\n");
+    fwrite($fp, $email." ".$name." ".$message."\n");
     fflush($fp);            // flush output before releasing the lock
         //flock($fp, LOCK_UN);    // release the lock
     //}
